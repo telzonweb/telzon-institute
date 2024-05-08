9b367b2bacb72eea1afbcaff1d6cb1f60de4d421
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-05-08 12:35:45 +0530

    form api update

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,34 +1,33 @@
-document.getElementById('myForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // prevent default form submission
+document.getElementById("myForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // prevent default form submission
 
-    // Get form data
-    var formData = new FormData(this);
+  // Get form data
+  var formData = new FormData(this);
 
-    // Convert formData to JSON
-    var jsonData = {};
-    formData.forEach(function(value, key) {
-        jsonData[key] = value;
+  // Convert formData to JSON
+  var jsonData = {};
+  formData.forEach(function (value, key) {
+    jsonData[key] = value;
+  });
+
+  console.log(jsonData);
+
+  // Make API call
+  fetch("https://telzonmarketing-in.onrender.com/api/landing/form", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jsonData),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // Handle API response
+      document.getElementById("response").innerText = data.message;
+    })
+    .catch(function (error) {
+      console.error("Error:", error);
     });
-
-    console.log(jsonData);
-
-    // Make API call
-    // fetch('https://studiovidha.onrender.com', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(jsonData)
-    // })
-    // .then(function(response) {
-    //     return response.json();
-    // })
-    // .then(function(data) {
-    //     // Handle API response
-    //     document.getElementById('response').innerText = data.message;
-    // })
-    // .catch(function(error) {
-    //     console.error('Error:', error);
-    // });
 });
-
